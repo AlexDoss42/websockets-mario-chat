4939306830235f4cf890f9dd6c097146338b6ee4
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-05-07 12:06:25 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(4001, function(){
+  console.log('listening to requests on port 4001')
+});
+
+//Static files
+
+app.use(express.static('public'));
+
+// Socket setup
+// takes a parameter which is the server we want to work with
+
+const io = socket(server);
+
+// Every user has a unique socket and the socket variable refers to each users' socket
+// connection is the connection to the server 
+
+io.on('connection', function(socket){
+  console.log('Made contact with the socket', socket.id);
+
+  //Handle Chat Event
+  socket.on('chat', function(data){
+    io.sockets.emit('chat', data);
+  });
+
+  //Handle Typing Event
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data)
+  });
+
+});
